8f000589bfbf750c10b5ed1f5a084a181e764684
zyh <2422634506@qq.com>
2017-02-27 14:16:03 +0800

    TouchDelagate实现扩大触屏范围

--- app/src/main/java/com/show/zyh/show_time/MainActivity.java
+++ app/src/main/java/com/show/zyh/show_time/MainActivity.java
@@ -2,6 +2,10 @@ package com.show.zyh.show_time;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.show.zyh.show_time.utils.TouchDelegates;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View view=findViewById(R.id.extend_view);
+
+        pudateViewTouchDegree(view);
+    }
+
+    private void pudateViewTouchDegree(View  view) {
+        TouchDelegates.extendTouchRang(400,view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"测试TouchDelegate这个类实现扩大Touch范围",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
