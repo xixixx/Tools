9070f964c79a39b68380b600b294c867e271ba08
zyh <2422634506@qq.com>
2017-03-06 19:33:19 +0800

    微信支付  Please enter the commit message for your changes. Lines starting

---
+++ app/src/main/java/com/show/zyh/show_time/http/RetrofitService.java
@@ -0,0 +1,9 @@
+package com.show.zyh.show_time.http;
+
+/**
+ * Created by zyh(2422634506@qq.com)
+ * 2017：13：23
+ */
+
+public class RetrofitService {
+}
---
+++ app/src/main/java/com/show/zyh/show_time/pay/WXPayEntryActivity.java
@@ -0,0 +1,9 @@
+package com.show.zyh.show_time.pay;
+
+/**
+ * Created by zyh(2422634506@qq.com)
+ * 2017：14：54
+ */
+
+public class WXPayEntryActivity {
+}
